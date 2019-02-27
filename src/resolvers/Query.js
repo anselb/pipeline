ced7b8bc639dcba773344ada03d6685b88c57abb
Ansel Bridgewater <iamansel@gmail.com>
2019-02-27 11:04:15 -0800

    Added query for users and user

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -30,7 +30,39 @@ async function job (parent, args, context, info) {
     })
 }
 
+async function users (parent, args, context, info) {
+  // const userId = getUserId(context)
+
+  return db.User
+    .findAll()
+    .then((users) => {
+      console.log("Got Users: ", users[0])
+      return users
+    })
+    .catch((err) => {
+      throw new Error(`Error: ${err}`)
+    })
+}
+
+async function user (parent, args, context, info) {
+  // const userId = getUserId(context)
+
+  return db.User
+    .findOne({
+      where: { id: args.id }
+    })
+    .then((user) => {
+      console.log("Got User: ", user)
+      return user
+    })
+    .catch((err) => {
+      throw new Error(`Error: ${err}`)
+    })
+}
+
 module.exports = {
+  users,
+  user,
   jobs,
   job,
 }
